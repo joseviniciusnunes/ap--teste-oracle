f480bc49b111492cf3088ee666755250ed1acd73
Jose Vinicius <joseviniciuus@outlook.com>
2021-07-20 17:00:43 -0300

    Variaveis de ambiente

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,16 @@
 FROM node:14-buster-slim
 
+ENV NODE_ENV=production
 ENV LD_LIBRARY_PATH=/app/oracle
 ENV ORACLE_HOME=/app/oracle
+
+#Credenciais do oracle
+ENV ORACLE_HOST=172.21.138.34
+ENV ORACLE_PORT=49161
+ENV ORACLE_USER=system
+ENV ORACLE_PASSWORD=oracle
+ENV ORACLE_SID=XE
+ENV ORACLE_SCHEMA=HR
 
 WORKDIR /app
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,14 +8,19 @@ import { JobModule } from './app/job/job.module';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            ignoreEnvFile: process.env.NODE_ENV === 'production',
+        }),
         TypeOrmModule.forRoot({
             type: 'oracle',
-            host: '172.21.138.34',
-            port: 49161,
-            sid: 'xe',
-            username: 'system',
-            password: 'oracle',
-            schema: 'HR',
+            host: process.env.ORACLE_HOST,
+            port: parseInt(process.env.ORACLE_PORT),
+            sid: process.env.ORACLE_SID,
+            schema: process.env.ORACLE_SCHEMA,
+            username: process.env.ORACLE_USER,
+            password: process.env.ORACLE_PASSWORD,
+            /*connectString:
+                '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=172.21.138.34)(PORT=49161))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=XE)))',*/
             entities: [Job],
             synchronize: false,
             logging: true,
